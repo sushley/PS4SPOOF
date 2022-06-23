ca94e1e2c479f475167903d1278a27809fcf7cf8
ᴇᴅɢʏ ᴋɪᴅᴅᴏ <107470957+Shino9d@users.noreply.github.com>
2022-06-23 11:20:05 +0200

    ERP <3

--- Source/Main.c
+++ Source/Main.c
@@ -1,7 +1,7 @@
 #include "ps4.h"
 #define SPOOF_FW 0x505003100 //(5.05)
                  
-////Credits to zerofo\\\\\
+////Credits to zerofo
 
 ///////* examples *\\\\\\\\
 
@@ -15,6 +15,9 @@
 //PS4 FW 0x4550.01100 (4.55)
 //PS4 FW 0x4050001000 (4.05)
 //PS4 FW 0x5050031000 (5.05)
+//PS4 FW 0x9600079000 (9.60)
+//PS4 FW 0x1760001000 (1.76)
+//PS4 FW 0x3550001000 (3.55)
 
 int _main(struct) thread *td) {
     UNUSED(td);
